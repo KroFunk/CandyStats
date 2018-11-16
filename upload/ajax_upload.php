f2f0fff9527ab41f68cd82f91cc699ffe7b658f1
Robin <robin@chalkcreation.co.uk>
2018-11-16 14:19:34 +0000

    Update ajax_upload.php

--- upload/ajax_upload.php
+++ upload/ajax_upload.php
@@ -1,14 +1,13 @@
 <?php
+require "../resources/config.php";
 $arr_file_types = ['application/octet-stream'];
  
 if (!(in_array($_FILES['file']['type'], $arr_file_types))) {
     echo "Error: Only .log files are supported 'application/octet-stream'.\n\nFile provided: " . $_FILES['file']['type'];
     return;
 }
- 
-if (!file_exists('uploads')) {
-    mkdir('uploads', 0777);
-}
+
+
  
 move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . time() . $_FILES['file']['name']);
  
