513174592f2c8c1c0bca6d2835fb80b5b95a0804
Robin <robin@chalkcreation.co.uk>
2018-12-04 17:16:37 +0000

    alignment fix

--- API/GET/awards/datatables.php
+++ API/GET/awards/datatables.php
@@ -9,12 +9,12 @@ $datatableOutput = '{ "data": [';
 //Batman award, most assisted kills
 $queryString = "SELECT `name`, `SteamID`, count(*) as AssistedKills FROM (SELECT `name`, `SteamID`, count(*) as assistedKills FROM `logdata` WHERE `EventType` = 'killed' OR `EventType` = 'assisted killing' GROUP BY `TIMESTAMP`, `EventVariable` HAVING count(*) >= 2) as assistedList GROUP BY `SteamID` ORDER BY `AssistedKills` DESC Limit 1";
 $result = mysqli_fetch_array(mysqli_query($con,$queryString));
-$datatableOutput .= '[ "<img src='."'".'resources/images/awards/batman-32x32.png'."'".' /> Batman","'.$result['name'].'","Most assisted kills: '.$result['AssistedKills'].'"], ';
+$datatableOutput .= '[ "<img style='."'vertical-align:middle;'".' src='."'".'resources/images/awards/batman-32x32.png'."'".' /> Batman","'.$result['name'].'","Most assisted kills: '.$result['AssistedKills'].'"], ';
 
 //Robin award, most assists
 $queryString = "SELECT `name`,count(*) as Assists FROM `logdata` WHERE `EventType` LIKE 'assisted killing%' GROUP BY SteamID ORDER BY Assists DESC LIMIT 1";
 $result = mysqli_fetch_array(mysqli_query($con,$queryString));
-$datatableOutput .= '[ "<img src='."'".'resources/images/awards/robin-32x32.png'."'".' /> Robin","'.$result['name'].'","Most kill assists: '.$result['Assists'].'"]';
+$datatableOutput .= '[ "<img style='."'vertical-align:middle;'".' src='."'".'resources/images/awards/robin-32x32.png'."'".' /> Robin","'.$result['name'].'","Most kill assists: '.$result['Assists'].'"]';
 
 $datatableOutput .= '] }';
 
