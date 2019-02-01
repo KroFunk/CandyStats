fc1082bab13b65f7b3e9868f8f9a769e2b54ce72
Robin <robin@chalkcreation.co.uk>
2019-02-01 15:48:34 +0000

    added a bar graph

--- PKPie.php
+++ PKPie.php
@@ -24,6 +24,8 @@
 
       // Set a callback to run when the Google Visualization API is loaded.
       google.charts.setOnLoadCallback(drawChart);
+      google.charts.setOnLoadCallback(drawWeaponChart);
+
 
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
@@ -52,6 +54,9 @@
 
         // Set chart options
         var options = {'title':'Player Kills',
+                        backgroundColor: '#171A1C',
+                        legend: {position: 'right', textStyle: {color: '#B3B4AE', fontSize: 12}},
+                        titleTextStyle: {color: '#FFF6EF', fontName: 'Arial', fontSize: 24, bold: 0},
                        'width':400,
                        'height':300};
 
@@ -59,6 +64,37 @@
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
+
+      // Callback that draws the weapon bar graph.
+      function drawWeaponChart() {
+
+      // Create the data table
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Weapon');
+      data.addColumn('number', 'Kills');
+      data.addRows([
+        ['AK47', 2],
+        ['P250', 2],
+        ['Negev', 2],
+        ['Knife', 1],
+        ['Grenade', 3]
+      ]);
+
+      // Set options
+      var options = {title:'Weapon Kills',
+                    backgroundColor: '#171A1C',
+                    legend: {position: 'right', textStyle: {color: '#B3B4AE', fontSize: 12}},
+                    titleTextStyle: {color: '#FFF6EF', fontName: 'Arial', fontSize: 24, bold: 0},
+                    legend: { position: "none" },
+                    width:400,
+                    height:300};
+
+      // Instantiate and draw the chart for Anthony's pizza.
+      var chart = new google.visualization.BarChart(document.getElementById('graph_div'));
+      chart.draw(data, options);
+      }
+
+      //SELECT `Misc_1` as 'Weapon', Count(*) as 'Kills' FROM `logdata` WHERE `SteamID` LIKE 'STEAM_1:0:445846' AND `EventType` LIKE 'killed' GROUP BY Misc_1 ORDER BY `Weapon` ASC
     </script>
   
 
@@ -68,6 +104,8 @@
 
 <!--Div that will hold the pie chart-->
 <div id="chart_div"></div>
+<div id="graph_div"></div>
+
 
 </body>
 
